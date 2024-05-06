b96b137e89d33e4f5bd5ba0dbf03b1e7462279cd
JulhyCar <julhy.carvalho@users.noreply.github.com>
2024-05-05 22:37:21 -0300

    Índice de Atividades
    Página de direcionamento para as atividades.

---
+++ meu-app/src/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Link}  from 'react-router-dom';
+
+const Home = () =>{
+    return(
+        <div>
+                <h1> Página Inicial</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/Ativ01">Atividade 01</Link>
+                    </li>
+                    <li>
+                        <Link to="/Ativ02">Atividade 02</Link>
+                    </li>
+                    <li>
+                        <Link to="/Ativ03">Atividade 03</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+export default Home;
